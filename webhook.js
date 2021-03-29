be16ff423bd859d4575e9b5bc426df755847a15e
shodgetts <stephenhodgetts91@gmail.com>
2021-03-29 16:17:34 -0600

    first pass at Zendesk

--- webhook.js
+++ webhook.js
@@ -6,6 +6,12 @@ const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const encoded = base64.encode(
+  "stephen@getstream.io/token:rQ2CCFPefNFOGwaHKbmH4ngCCl9kbSAjPbWeBDUN"
+);
+
+const startingURL = "https://getstream.zendesk.com/api/v2/tickets/";
+
 //chat client
 const StreamChat = require("stream-chat").StreamChat;
 const chatClient = StreamChat.getInstance(
@@ -31,9 +37,27 @@ app.post("/", (req, res) => {
     if (parsedBody.type === "channel.updated") {
       const { channel_type, channel_id } = parsedBody;
       const channel = chatClient.channel(channel_type, channel_id);
-      const state = await channel.query({messages: {limit: 40}})
-      const {messages} = state
-      console.log(messages)
+      const state = await channel.query({ messages: { limit: 40 } });
+      const { messages } = state;
+      const lines = "";
+      messages.forEach((mes) => (lines += `${mes.text} - ${mes.user.id} \n`));
+      console.log(lines);
+      const raw = await fetch(`https://getstream.zendesk.com/api/v2/tickets`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${encoded}`,
+        },
+        body: {
+          ticket: {
+            comment: {
+              body: lines,
+            },
+            priority: "normal",
+            subject: "New Dispute",
+          },
+        },
+      });
     }
     res.status(200).send("OK");
   });
